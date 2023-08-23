d0b4e0539f1be980f2f8d10297150567bc01c164
Vinayak.H <88961285+vinayak-h@users.noreply.github.com>
2023-08-23 22:21:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,27 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route("/calculator/greeting", methods=['GET'])
+# def greeting():
+#     return ''
+
+# @app.route("/calculator/add", methods=['POST'])
+# def add():
+#     return ''
+
+# @app.route("/calculator/subtract", methods=['POST'])
+# def subtract():
+#     return ''
+
+# if __name__ == '__main__':
+#     app.run(port=8080,host='0.0.0.0')
+
+
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
@@ -16,4 +36,5 @@ def subtract():
     return ''
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
+
